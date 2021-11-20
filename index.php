54d547bdf5a4a074bee3ea45356b02e0e7d1938d
Tiago França <tiago@tiagofranca.com>
2021-11-20 18:35:02 -0300

    Melhorias na usabilidade e no design
    Mudanças a serem submetidas:
    	modified:   index.php

--- index.php
+++ index.php
@@ -7,18 +7,50 @@
     <title>Document</title>
     <style>
         * { margin: 0; padding: 0; }
-        /* [todo-item]{ color: red; width: 50%; } */
-        ul[todo-item-container] > li[data-item-target]{ background: red !important; display: flex; flex-direction: row;}
+        [todo-item]{ color: red; width: 50%; }
+
+        button {
+            background: #ccc;
+            border: none;
+            padding: 10px;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+
+        ul[todo-item-container] > li[data-item-target] > button[todo-remove] { background: #db0e0ec2; color: white; border-radius: 0; margin-right: 22px; padding: 15px;}
+        ul[todo-item-container] > li[data-item-target] > button[todo-remove]:hover { background: #741d07;}
+        /* ul[todo-item-container] > li[data-item-target]{ display: flex; flex-direction: row;} */
         ul[todo-item-container] > li[data-item-target].done, li[data-item-target].done {  background: green !important; }
-    </style>
+
+        .d-flex { display: flex; }
+        .d-flex.flex-dir-row { flex-direction: row;}
+        .d-flex.flex-wrap { flex-wrap: wrap; }
+        .d-flex.flex-justify-start { justify-content: flex-start; }
+        .d-flex.flex-justify-end { justify-content: flex-end; }
+        .d-flex.flex-align-center { align-items: center; }
+
+        .flex-container-left{ display: flex; flex-direction: row; justify-content: flex-start; align-items: center; flex-wrap: wrap; }
+
+        ul[todo-item-container] { padding: 3px 5px; margin: 3px 0; }
+        ul[todo-item-container] > li { margin: 7px 0px; background: antiquewhite}
+
+        .flex-container-left input[type=radio], .flex-container-left input[type=checkbox]{    display:none; /* Esconde os inputs */}.flex-container-left label {    cursor: pointer;}.flex-container-left input[type="radio"] + label:before, .flex-container-left input[type="checkbox"] + label:before {    border: 1px solid #5D5C5C;    content: "\00a0";    display: inline-block;    font: 16px/1em sans-serif;    height: 16px;    margin: 0 .25em 0 0;    padding:0;    vertical-align: top;    width: 16px;    border-radius:4px;}.flex-container-left input[type="radio"]:checked + label:before, .flex-container-left input[type="checkbox"]:checked + label:before {    background: #A0A0A0;    color: #FFF;    content: "\2713";    text-align: center;}.flex-container-left input[type="radio"]:checked + label:after, .flex-container-left input[type="checkbox"]:checked + label:after {    font-weight: bold;}
+
+        .flex-container-left input[type=checkbox] + label:before { padding: 0.2rem; margin-right: 0.3rem; }
+        .flex-container-left input[type=text][todo-title], .flex-container-left span[todo-title-text] {margin: 0px 1rem; padding: 12px 0px;}
+
+        .checklist-container {margin: 5rem; box-shadow: #afa496 1px 1px 8px; border-radius: 5px; padding: 8px;}
+        </style>
 </head>
 <body>
-    <form method="POST" action="form_action.php">
-        <button type="button" todo-item-new>[ + ]</button>
+    <div class="checklist-container">
+        <form method="POST" action="form_action.php">
+            <button type="button" todo-item-new>[ + ]</button>
 
-        <ul todo-item-container></ul>
-        <button type="submit">Enviar</button>
-    </form>
+            <ul todo-item-container></ul>
+            <button type="submit">Enviar</button>
+        </form>
+    </div>
 
     <script>
         function uuidv4()
@@ -26,6 +58,11 @@
             return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
                 (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
             );
+        }
+
+        function getItemId(element)
+        {
+            return element.dataset.itemTarget;
         }
 
         function genRandId()
@@ -64,6 +101,7 @@
             };
 
             var remove_todo_item = document.createElement('button');
+                remove_todo_item.setAttribute('todo-remove', rand_id);
                 remove_todo_item.setAttribute('data-item-target', rand_id);
                 remove_todo_item.setAttribute('type', "button");
                 remove_todo_item.innerText = '[ - ]';
@@ -76,6 +114,7 @@
                 todo_item.value = JSON.stringify(initial_data);
 
             var todo_title = document.createElement('span');
+                todo_title.setAttribute('todo-title-text', "");
                 todo_title.setAttribute('data-item-target', rand_id);
                 todo_title.innerText = initial_data.title;
 
@@ -83,44 +122,138 @@
                     todo_toggle.setAttribute('type', "checkbox");
                     todo_toggle.setAttribute('data-item-target', rand_id);
                     todo_toggle.setAttribute('todo-toggle', "");
+                    todo_toggle.id = `ckbx_${rand_id}`
                     todo_toggle.checked = initial_data.done;
 
+            var todo_toggle_label = document.createElement('label');
+                todo_toggle_label.setAttribute('data-item-target', rand_id);
+                todo_toggle_label.setAttribute('for', `ckbx_${rand_id}`);
+                todo_toggle_label.classList.add("exemplo2");
+                /* todo_toggle_label.appendChild(todo_toggle); */
 
             var todo_title_input = document.createElement('input');
+                todo_title_input.setAttribute('type', 'text');
                 todo_title_input.setAttribute('data-item-target', rand_id);
                 todo_title_input.setAttribute('todo-title', "");
+                todo_title_input.setAttribute('title', "Pressione enter para salvar as alterações");
                 todo_title_input.style.display = "none";
                 todo_title_input.value = initial_data.title;
 
 
             var todo_li = document.createElement('li');
+                todo_li.classList.add('flex-container-left');
                 todo_li.setAttribute('data-item-target', rand_id);
-                todo_li.prepend(remove_todo_item);
                 todo_li.prepend(todo_item);
                 todo_li.prepend(todo_title_input);
                 todo_li.prepend(todo_title);
+                todo_li.prepend(todo_toggle_label);
                 todo_li.prepend(todo_toggle);
+                todo_li.prepend(remove_todo_item);
 
                 initial_data.done  ? todo_li.classList.add('done')
                                     : todo_li.classList.remove('done');
 
+            addListenersToTodoTitle(todo_title);
             addListenerToRemoveTodo(remove_todo_item);
             addListenerToTodoDataToogle(todo_toggle);
-            addListenerToTodoDataTitle(todo_title_input);
+            addListenerToTodoTitleInput(todo_title_input);
 
             todo_container.prepend(todo_li);
         }
 
-        function addListenerToTodoDataTitle(todo_title_input)
+        function addListenersToTodoTitle(todo_title)
         {
+            //verifica se o item é um elemento
+            if(!todo_title || !(todo_title instanceof HTMLElement)) return;
+
+            todo_title.addEventListener('click', e => {
+                var item_id = getItemId(e.target);
+                    if(!item_id) return;
+
+                toggleEditItem(item_id);
+            });
+        }
+
+        function listenEscPressOnTitleInput(element)
+        {
+            if(!element || !(element instanceof HTMLElement)) return;
+
+            // document.querySelector('input[data-item-target][todo-title]').addEventListener('keydown', function(e) {
+            element.addEventListener('keydown', function(e) {
+                if (e.keyCode == 27)
+                {
+                    alert("Escape");
+                }
+            });
+        }
+
+        function getInputTitleValues(target)
+        {
+            var item_id = getItemId(target);
+                    if(!item_id) return;
+
+            var todo_target = document.querySelector(`[todo-item="${item_id}"]`);
+            var target_data = JSON.parse(todo_target.value);
+            target_data.title = target.value;
+            todo_target.value = JSON.stringify(target_data);
+
+            var todo_item_text = document.querySelector(`span[data-item-target="${item_id}"]`);
+            if (todo_item_text)
+                todo_item_text.innerText = target.value;
+        }
+
+        function resetTitleInput(target)
+        {
+            var item_id = getItemId(target);
+                    if(!item_id) return;
+
+            var todo_target = document.querySelector(`[todo-item="${item_id}"]`);
+            var target_data = JSON.parse(todo_target.value);
+            target.value = target_data.title;
+        }
+
+        function addListenerToTodoTitleInput(todo_title_input)
+        {
+            // listenEscPressOnTitleInput(todo_title_input);
+
+            todo_title_input.addEventListener('keydown', function(e) {
+                if (e.keyCode == 27)//Esc
+                {
+                    var item_id = getItemId(e.target);
+                    if(!item_id) return;
+
+                    resetTitleInput(e.target);
+
+                    toggleEditItem(item_id);
+                }
+
+                if (e.keyCode == 13)//Enter
+                {
+                    e.preventDefault();
+                    var item_id = getItemId(e.target);
+                    if(!item_id) return;
+
+                    getInputTitleValues(e.target);
+                    toggleEditItem(item_id);
+                }
+            });
+
+            todo_title_input.addEventListener('focusout', e => {
+                var item_id = getItemId(e.target);
+                    if(!item_id) return;
+
+                toggleEditItem(item_id)
+            });
+
+            /**
+             * Ignorando o evento de change na ateração do titulo do TODO item
+             * para que a alteração seja feita apenas quando o usuário confirmar com um enter
+             *
             todo_title_input.addEventListener('change', e => {
-                var target = e.target;
-                var todo = target.dataset.itemTarget;
-                var todo_target = document.querySelector(`[todo-item="${todo}"]`);
-                var target_data = JSON.parse(todo_target.value);
-                target_data.title = target.value;
-                todo_target.value = JSON.stringify(target_data);
+                var target  = e.target;
+                getInputTitleValues(target);
             })
+            /**/
         }
 
         function addListenerToTodoDataToogle(todo_toggle)
@@ -180,13 +313,50 @@
             });
         }
 
+        function toggleEditItem(item_id)
+        {
+            if(!item_id || item_id.length < 4) return;
+
+            var todo_item_text = document.querySelector(`span[data-item-target="${item_id}"]`);
+            if (!todo_item_text) return;
+
+            todo_item_text.style.display === "" ? todo_item_text.style.display = "none" : todo_item_text.style.display = "";
+
+            var todo_item_title_input = document.querySelector(`input[data-item-target="${item_id}"][todo-title]`);
+            if (!todo_item_title_input) return;
+
+            todo_item_title_input.style.display === "" ? todo_item_title_input.style.display = "none" : todo_item_title_input.style.display = "";
+        }
+
+        function addListenerToTodoItemText(todo_item_text)
+        {
+            if (!todo_item_text)
+            {
+                return;
+            }
+
+            todo_item_text.addEventListener('click', e => {
+                var target = e.target;
+
+                var item_id = getItemId(el.target);
+                if(!item_id) return;
+
+                var todo_target = document.querySelector(`[todo-item="${item_id}"]`);
+                if (!todo_target) return;
+
+                var todo_item = document.querySelector(`li[data-item-target="${item_id}"]`);
+
+                toggleEditItem(item_id);
+            });
+        }
+
         document.addEventListener('DOMContentLoaded', function() {
             document.querySelectorAll('[todo-toggle]').forEach(todo_toggle => {
                 addListenerToTodoDataToogle(todo_toggle);
             });
 
             document.querySelectorAll('[todo-title]').forEach(todo_title_input => {
-                addListenerToTodoDataTitle(todo_title_input);
+                addListenerToTodoTitleInput(todo_title_input);
             });
 
             document.querySelectorAll('[todo-item-new]').forEach(add_todo => {
